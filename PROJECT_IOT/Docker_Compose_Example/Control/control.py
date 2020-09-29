ffb69f4546e26efdd84f1b2671c934bd43c07410
eyas-UC <49149183+eyas-UC@users.noreply.github.com>
2020-09-29 13:05:39 +0200

    edited the registration part

--- PROJECT_IOT/Docker_Compose_Example/Control/control.py
+++ PROJECT_IOT/Docker_Compose_Example/Control/control.py
@@ -2,30 +2,8 @@
 #periodically check door-bell (push-button) (subscribe to device connector of push button)
 # if Harr says that there is a face notify the owner of an existance of a face.
 
+##### I think we should use multi threading for each task
 
-"""{
-              "type": "REST_API",
-              "title": "image_capture",
-              "description": "image capture",
-              "meta": {},
-              "apis": [
-                {
-                  "id": "4",
-                  "title": "im_cap",
-                  "description": "string",
-                  "protocol": "string",
-                  "url": "localhost:8088",
-                  "spec": {
-                    "mediaType": "string",
-                    "url": "string",
-                    "schema": {}
-                  },
-                  "meta": {}
-                }
-              ],
-              "doc": "string",
-              "ttl": 3
-            }"""
 
 
 
@@ -34,8 +12,39 @@
 import json
 import requests
 import service_search
-# with open('config_control.json','r') as file:
-#     dict = json.load(file)
-#     file.close()
+from threading import *
+from registration import *
+
 
 url = service_search.search('http://localhost:8082','mqtt-broker-at-localhost')
+
+class Controller:
+    def __init__(self):
+        with open('initialization.json', 'r') as file:
+            dict = json.load(file)
+        file.close()
+        #check if the right service catalog is up
+        try:
+            if requests.get(dict['sc_url']).status_code == 200:
+                print('the service catalog is up and running')
+        except:
+            print('the service catalog is down')
+
+    def get_data(self):
+        pass
+
+    def action(self):
+        pass
+
+    def user_interface(self):
+        pass
+
+
+
+
+
+
+
+
+a_a = Controller()
+
---
+++ PROJECT_IOT/Docker_Compose_Example/Control/registration.py
@@ -0,0 +1,28 @@
+import json
+import requests
+import time	
+
+
+
+def registration(json_file, url):
+	# Import informations and metadata from haar.json 
+	obj = json.load(open(json_file))
+
+	while True:
+		try:
+			x = requests.post(url, obj)
+			ID = json.loads(x.text)['id']
+			print(f'id is {ID}')
+			print('registration succedeed\nID is {}'.format(ID))
+			while True:
+			    try:
+			        x = requests.put(url + ID, obj)
+			        ID = json.loads(x.text)['id']
+			        print('update succeeded\nID is {}'.format(ID))
+			        time.sleep(5)
+			    except:
+			    	print('update failed with the host')
+
+		except:
+			print('registration failed with the host')
+		time.sleep(5)
